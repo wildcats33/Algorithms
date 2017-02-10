dd74933c7a3136d9077e6d38f8e6cf3f9c8dff67
Maneesh R <maneesh@ksu.edu>
2017-02-09 23:44:30 -0500

    MaximumSumFromRootToLeaf

---
+++ MaximumSumFromRootToLeaf.cs
@@ -0,0 +1,12 @@
+// This is post order traversal
+public int MaximumSumFromRootToLeaf(Node n)
+{
+    if(n == null) {
+        return 0;
+    }
+
+    readonly int maximumLeft = MaximumSumFromRootToLeaf(n.Left);
+    readonly int maximumRight = MaximumSumFromRootToLeaf(n.Right);
+
+    return n.Value + ((maximumLeft > maximumRight) ?  + maximumLeft : maximumRight);
+}
